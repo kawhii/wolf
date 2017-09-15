48e795d57ad4f3eac9d27874b24b7a9231795b7f
laomazi2006 <756884434@qq.com>
2017-09-16 00:27:44 +0800

    添加权限管理模块

---
+++ wolf-permission-management/src/main/java/com/carl/wolf/permission/PermissionManagementApplication.java
@@ -0,0 +1,20 @@
+/*
+ * 版权所有.(c)2008-2017. 卡尔科技工作室
+ */
+
+package com.carl.wolf.permission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Carl
+ * @date 2017/9/16
+ * @since 1.0.0
+ */
+@SpringBootApplication
+public class PermissionManagementApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PermissionManagementApplication.class, args);
+    }
+}
