aab3a89264797854244765ca0935e38841304901
laomazi2006 <756884434@qq.com>
2017-09-17 13:12:04 +0800

    可以生成jwt token但校验失败，加解密配置还需要调整

--- wolf-permission-management/src/main/java/com/carl/wolf/permission/config/ShiroConfiguration.java
+++ wolf-permission-management/src/main/java/com/carl/wolf/permission/config/ShiroConfiguration.java
@@ -4,6 +4,8 @@
 
 package com.carl.wolf.permission.config;
 
+import com.nimbusds.jose.EncryptionMethod;
+import com.nimbusds.jose.JWEAlgorithm;
 import io.buji.pac4j.filter.CallbackFilter;
 import io.buji.pac4j.filter.LogoutFilter;
 import io.buji.pac4j.filter.SecurityFilter;
@@ -18,14 +20,25 @@ import org.apache.shiro.spring.web.config.AbstractShiroWebFilterConfiguration;
 import org.apache.shiro.spring.web.config.DefaultShiroFilterChainDefinition;
 import org.apache.shiro.spring.web.config.ShiroFilterChainDefinition;
 import org.pac4j.cas.client.CasClient;
+import org.pac4j.cas.client.rest.CasRestFormClient;
 import org.pac4j.cas.config.CasConfiguration;
+import org.pac4j.cas.config.CasProtocol;
 import org.pac4j.core.client.Clients;
 import org.pac4j.core.config.Config;
+import org.pac4j.http.client.direct.ParameterClient;
+import org.pac4j.jwt.config.encryption.ECEncryptionConfiguration;
+import org.pac4j.jwt.config.encryption.SecretEncryptionConfiguration;
+import org.pac4j.jwt.config.signature.RSASignatureConfiguration;
+import org.pac4j.jwt.config.signature.SecretSignatureConfiguration;
+import org.pac4j.jwt.credentials.authenticator.JwtAuthenticator;
+import org.pac4j.jwt.profile.JwtGenerator;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.servlet.Filter;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -38,10 +51,15 @@ import java.util.Map;
  */
 @Configuration
 public class ShiroConfiguration extends AbstractShiroWebFilterConfiguration {
+    @Value("#{ @environment['cas.prefixUrl'] ?: null }")
+    private String prefixUrl;
     @Value("#{ @environment['cas.loginUrl'] ?: null }")
     private String casLoginUrl;
     @Value("#{ @environment['cas.callbackUrl'] ?: null }")
     private String callbackUrl;
+
+    @Value("${jwt.salt}")
+    private String salt;
 
     @Bean
     public Realm pac4jRealm() {
@@ -55,16 +73,56 @@ public class ShiroConfiguration extends AbstractShiroWebFilterConfiguration {
     @Bean
     public Filter casSecurityFilter() {
         SecurityFilter filter = new SecurityFilter();
-        filter.setClients("CasClient");
+        filter.setClients("CasClient,form,jwt");
         filter.setConfig(casConfig());
         return filter;
     }
 
     @Bean
+    protected JwtGenerator jwtGenerator(JwtAuthenticator jwtAuthenticator) {
+        JwtGenerator jwtGenerator = new JwtGenerator();
+        jwtAuthenticator.setEncryptionConfigurations(jwtAuthenticator.getEncryptionConfigurations());
+        jwtAuthenticator.setSignatureConfigurations(jwtAuthenticator.getSignatureConfigurations());
+        return jwtGenerator;
+//        return new JwtGenerator(new SecretSignatureConfiguration(salt), new SecretEncryptionConfiguration(salt));
+    }
+
+
+
+
+    @Bean
     protected Clients clients() {
         Clients clients = new Clients();
-        clients.setClients(casClient());
+        CasRestFormClient casRestFormClient = new CasRestFormClient();
+        casRestFormClient.setConfiguration(casConfiguration());
+        casRestFormClient.setName("form");
+        ParameterClient parameterClient = new ParameterClient("token", jwtAuthenticator());
+        parameterClient.setSupportGetRequest(true);
+        parameterClient.setName("jwt");
+        clients.setClients(casClient(), casRestFormClient, parameterClient);
         return clients;
+    }
+
+    @Bean
+    protected JwtAuthenticator jwtAuthenticator() {
+        JwtAuthenticator jwtAuthenticator = new JwtAuthenticator();
+        try {
+            KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+            KeyPair rsaKeyPair = keyGen.generateKeyPair();
+            jwtAuthenticator.addSignatureConfiguration(new SecretSignatureConfiguration(salt));
+//            jwtAuthenticator.addSignatureConfiguration(new RSASignatureConfiguration(rsaKeyPair));
+
+            keyGen = KeyPairGenerator.getInstance("EC");
+            KeyPair ecKeyPair = keyGen.generateKeyPair();
+            ECEncryptionConfiguration encConfig = new ECEncryptionConfiguration(ecKeyPair);
+            encConfig.setAlgorithm(JWEAlgorithm.ECDH_ES_A128KW);
+            encConfig.setMethod(EncryptionMethod.A192CBC_HS384);
+            jwtAuthenticator.addEncryptionConfiguration(new SecretEncryptionConfiguration(salt));
+//            jwtAuthenticator.addEncryptionConfiguration(encConfig);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return jwtAuthenticator;
     }
 
     @Bean
@@ -77,6 +135,8 @@ public class ShiroConfiguration extends AbstractShiroWebFilterConfiguration {
     @Bean
     public CasConfiguration casConfiguration() {
         CasConfiguration casConfiguration = new CasConfiguration(casLoginUrl);
+        casConfiguration.setProtocol(CasProtocol.CAS30);
+        casConfiguration.setPrefixUrl(prefixUrl);
         return casConfiguration;
     }
 
--- wolf-permission-management/src/main/java/com/carl/wolf/permission/controller/IndexController.java
+++ wolf-permission-management/src/main/java/com/carl/wolf/permission/controller/IndexController.java
@@ -8,11 +8,22 @@
 
 package com.carl.wolf.permission.controller;
 
+import org.pac4j.core.context.J2EContext;
+import org.pac4j.core.profile.CommonProfile;
+import org.pac4j.core.profile.ProfileManager;
+import org.pac4j.jwt.profile.JwtGenerator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 /**
  * @author Carl
@@ -21,6 +32,9 @@ import javax.servlet.http.HttpServletRequest;
  */
 @RestController
 public class IndexController {
+
+    @Autowired
+    private JwtGenerator generator;
 
     @GetMapping("/")
     public Object index() {
@@ -35,5 +49,19 @@ public class IndexController {
     @GetMapping("/user/detail")
     public Object detail(HttpServletRequest request) {
         return "users:" + request.getUserPrincipal().getName();
+    }
+
+    @RequestMapping("/user/login")
+    public Object login(HttpServletRequest request, HttpServletResponse response) {
+        Map<String, Object> model = new HashMap<>();
+        J2EContext context = new J2EContext(request, response);
+        String token = "";
+        final ProfileManager manager = new ProfileManager(context);
+        final Optional<CommonProfile> profile = manager.get(true);
+        if (profile.isPresent()) {
+            token = generator.generate(profile.get());
+        }
+        model.put("token", token);
+        return new HttpEntity<>(model);
     }
 }
